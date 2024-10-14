401d33336fdff1e0285cbf5bbde01ff54520695b
zoeznm <bomikim046@gmail.com>
2024-10-15 02:32:08 +0900

    contact page 큰 구성 레이아웃 완성

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import '../styles/Contact.css';
+import React from "react";
+import "../styles/Contact.css";
 import Navbar from "../components/Navbar";
-import Topbar from '../components/Topbar';
+import Topbar from "../components/Topbar";
 
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
       <Navbar />
       <Topbar />
+      <div className="reset">
+        <button></button>
+      </div>
+      <div className="manylovethx">
+        <p>많은</p>
+        <p>관심</p>
+        <p>부탁드립니다.</p>
+      </div>
     </div>
   );
 };
