d823fa055cfc09d543cc685996ae38d9b2d84a3a
zoeznm <bomikim046@gmail.com>
2024-10-14 21:23:55 +0900

    연락 페이지 제작 완료

---
+++ portfolio/src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>여기에 연락처 정보를 작성하세요.</p>
+      <Link to="/">메인 페이지로 돌아가기</Link>
+    </div>
+  );
+};
+
+export default Contact;
