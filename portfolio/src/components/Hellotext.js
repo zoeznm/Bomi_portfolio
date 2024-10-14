7465374b068a6bc36ef3be1dec58a8e4d3d3b508
zoeznm <bomikim046@gmail.com>
2024-10-15 02:18:30 +0900

    aboutme page css 큰 구구성은 완료

---
+++ portfolio/src/components/Hellotext.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hellotext.css";
+
+const Hellotext = () => {
+  return (
+    <div className="hellotext">
+      <p>안녕하세요, 저는 김보미입니다.</p>
+      <p>Front-end Engineer</p>
+    </div>
+  );
+};
+
+export default Hellotext;
--- portfolio/src/pages/AboutMe.js
+++ portfolio/src/pages/AboutMe.js
@@ -1,14 +1,64 @@
-import React from 'react';
-import '../styles/AboutMe.css';
+import React from "react";
+import "../styles/AboutMe.css";
 import Navbar from "../components/Navbar";
-import Topbar from '../components/Topbar';
+import Topbar from "../components/Topbar";
+import Hellotext from "../components/Hellotext";
 
 const AboutMe = () => {
   return (
     <div>
-      <h1>About Me</h1>
       <Navbar />
       <Topbar />
+      <Hellotext />
+      <div>
+        <img
+          className="aboutme"
+          src={`${process.env.PUBLIC_URL}/aboutme.png`}
+          alt="김보미의 사진"
+        />
+        <div className="arrowimg_1">
+          <img
+            className="arrow arrow_1"
+            src={`${process.env.PUBLIC_URL}/arrow.png`}
+            alt="화살표"
+          />
+        </div>
+        <div className="arrowimg_2">
+          <img
+            className="arrow arrow_2"
+            src={`${process.env.PUBLIC_URL}/arrow.png`}
+            alt="화살표"
+          />
+        </div>
+        <div className="arrowimg_3">
+          <img
+            className="arrow arrow_3"
+            src={`${process.env.PUBLIC_URL}/arrow.png`}
+            alt="화살표"
+          />
+        </div>
+        <div className="arrowimg_4">
+          <img
+            className="arrow arrow_4"
+            src={`${process.env.PUBLIC_URL}/arrow.png`}
+            alt="화살표"
+          />
+        </div>
+        <div className="arrowimg_5">
+          <img
+            className="arrow arrow_5"
+            src={`${process.env.PUBLIC_URL}/arrow.png`}
+            alt="화살표"
+          />
+        </div>
+        <div className="arrowimg_6">
+          <img
+            className="arrow arrow_6"
+            src={`${process.env.PUBLIC_URL}/arrow.png`}
+            alt="화살표"
+          />
+        </div>
+      </div>
     </div>
   );
 };
