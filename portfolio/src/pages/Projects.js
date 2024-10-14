46f76d3093a9281af9143923f3b85edcb2e3bd8c
zoeznm <bomikim046@gmail.com>
2024-10-14 23:48:44 +0900

    레이아웃 수정 완료

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/Projects.css';
 import Navbar from "../components/Navbar";
 import Topbar from '../components/Topbar';
@@ -10,7 +9,6 @@ const Projects = () => {
       <h1>Projects</h1>
       <Navbar />
       <Topbar />
-      <Link to="/">메인 페이지로 돌아가기</Link>
     </div>
   );
 };
