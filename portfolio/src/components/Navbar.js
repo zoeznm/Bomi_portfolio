969c453c4c8a5fa3428d0466bdab919b69f02be0
zoeznm <bomikim046@gmail.com>
2024-10-14 21:22:31 +0900

    메뉴바 컴포넌트 제작 완료

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/aboutme">About Me</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
