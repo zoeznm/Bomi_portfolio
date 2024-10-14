62aac34867de257286650b50771362bbbf7612e0
zoeznm <bomikim046@gmail.com>
2024-10-14 21:23:06 +0900

    자기 소개 페이지 제작 완료

---
+++ portfolio/src/pages/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>여기에 본인에 대한 소개를 작성하세요.</p>
+      <Link to="/">메인 페이지로 돌아가기</Link>
+    </div>
+  );
+};
+
+export default AboutMe;
