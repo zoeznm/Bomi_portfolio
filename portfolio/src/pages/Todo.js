65e0959445357e41caed3a550bdd5957b88c7ee6
zoeznm <bomikim046@gmail.com>
2024-10-16 01:22:58 +0900

    이미지 슬라이드 구현 완료

--- portfolio/src/pages/Todo.js
+++ portfolio/src/pages/Todo.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Personal_todo.css";
 import Navbar from "../components/Navbar";
 import Topbar from "../components/Topbar";
 import { Link } from "react-router-dom";
 
 const Todo = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  const slides = [
+    "/todo1.png", // 여기에 실제 이미지 경로를 넣어야 해
+    "/todo2.png",
+    "/todo3.png"
+  ];
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
+  };
+
   return (
     <div>
       <Navbar />
@@ -32,6 +48,22 @@ const Todo = () => {
           <p>MySQL</p>
         </div>
       </div>
+      
+      {/* 슬라이더 섹션 */}
+      <div className="slider-container">
+        <div className="slider">
+          <img
+            src={slides[currentIndex]}
+            alt={`Slide ${currentIndex + 1}`}
+            className="slide"
+          />
+        </div>
+        <div className="buttons">
+          <button onClick={prevSlide}>Prev</button>
+          <button onClick={nextSlide}>Next</button>
+        </div>
+      </div>
+      
       <div className="todo_url">
         <a href="http://todocalendar.s3-website.ap-northeast-2.amazonaws.com/">
           http://todocalendar.s3-website.ap-northeast-2.amazonaws.com/
